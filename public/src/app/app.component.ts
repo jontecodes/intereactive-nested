4b8434755180ae199d96154354e798605e21cf5e
jontecodes <jonterufus@yahoo.com>
2019-06-14 13:11:40 -0500

    first commit

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -10,22 +10,44 @@ import { discardPeriodicTasks } from "@angular/core/testing";
 export class AppComponent implements OnInit {
   title = "Restful Tasks API";
   tasks = [];
-
+  all = [];
+  onButtonClick(event: any): void {
+    this.getTasksFromService();
+    console.log(event);
+  }
+  onShowClick(id) {
+    this.getOne(id);
+  }
 
   constructor(private _httpService: HttpService) {}
 
   ngOnInit(){
-    this.getTasksFromService()
+    // this.getTasksFromService()
+  }
+  getOne(id){
+    let obs = this._httpService.getSingleTask(id);
+    obs.subscribe(data => {
+      this.all = data['data'];
+      console.log(this.all);
+      console.log('Got some data', data.data);
+    })
   }
   getTasksFromService(){
     let observable = this._httpService.getTasks();
     observable.subscribe(data => {
       console.log('Got our data!', data);
-      for (let i = 0; i < data['data'].length; i++){
-        this.tasks.push(data['data'][i].title);
-      }
-      // this.tasks = data['data'][0].title;
-      console.log(this.tasks);
+      this.tasks = data['data'];
     })
   }
+  // getTasksFromService(){
+  //   let observable = this._httpService.getTasks();
+  //   observable.subscribe(data => {
+  //     console.log('Got our data!', data);
+  //     for (let i = 0; i < data['data'].length; i++){
+  //       this.tasks.push(data['data'][i].title);
+  //     }
+  //     // this.tasks = data['data'][0].title;
+  //     console.log(this.tasks);
+  //   })
+  // }
 }
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -8,18 +8,18 @@ export class HttpService {
 
   constructor(private _http: HttpClient) {
     // this.getTasks();
-    this.getSingleTask('5d01674801775e373484832b');
+    // this.getSingleTask('5d01674801775e373484832b');
   }
   getSingleTask(id){
     // hardcoded :id
 
-    let observable = this._http.get(`/tasks/${id}`);
-    observable.subscribe(data => console.log('Got one task', data));
+    return this._http.get(`/tasks/${id}`);
+    // observable.subscribe(data => console.log('Got one task', data));
   }
   getTasks(){
     // our http response is an observable. store it in the variable tempObserable
 
     // subscribe to out observable and provide the cose we would like to do with our data from the res
-    return this._http.get('/tasks')
+    return this._http.get('/tasks');
   }
 }
